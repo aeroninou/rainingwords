cbad6bc2fb4be80917edfddcba25d9fffcb5e233
Sergio Garcia <segarciat314@gmail.com>
2022-11-04 18:02:32 -0400

    Added tested setBackgroundColor method

--- src/com/word/util/Color.java
+++ src/com/word/util/Color.java
@@ -1,15 +1,21 @@
 package com.word.util;
 
 enum Color {
-    GREEN("[32m");
+    GREEN("[32m", "[42m");
 
     private final String fontCode;
+    private final String backgroundCode;
 
-    Color(String fontCode) {
+    Color(String fontCode, String backgroundCode) {
         this.fontCode = fontCode;
+        this.backgroundCode = backgroundCode;
     }
 
     public String setFontColor(String text) {
         return String.format("\033%s%s\033[0m", fontCode, text);
+    }
+
+    public String setBackgroundColor(String text) {
+        return String.format("\033%s%s\033[0m", backgroundCode, text);
     }
 }
--- test/com/word/util/ColorTest.java
+++ test/com/word/util/ColorTest.java
@@ -1,14 +1,26 @@
 package com.word.util;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ColorTest {
+    private String text = "hello";
+
     @Test
     public void setFontColor_shouldContainOriginalText() {
-        String text = "hello world";
         String greenText = Color.GREEN.setFontColor(text);
+        assertTrue(greenText.contains(text));
+        // regular text color.
+        System.out.println(text);
+        // should be green color.
+        System.out.println(greenText);
+    }
+
+    @Test
+    public void setBackgroundColor_shouldContainOriginalText() {
+        String greenText = Color.GREEN.setBackgroundColor(text);
         assertTrue(greenText.contains(text));
         // regular text color.
         System.out.println(text);
