05bd6b9a343f8b2f340de975df910775988d26d6
Sergio Garcia <segarciat314@gmail.com>
2022-11-04 17:54:07 -0400

    Added tested setFontColor method

--- src/com/word/util/Color.java
+++ src/com/word/util/Color.java
@@ -1,9 +1,15 @@
 package com.word.util;
 
 enum Color {
-    /**
-     * TODO: Sergio
-     *  - and jFrame panel
-     * */
+    GREEN("[32m");
 
+    private final String fontCode;
+
+    Color(String fontCode) {
+        this.fontCode = fontCode;
+    }
+
+    public String setFontColor(String text) {
+        return String.format("\033%s%s\033[0m", fontCode, text);
+    }
 }
--- test/com/word/util/ColorTest.java
+++ test/com/word/util/ColorTest.java
@@ -1,7 +1,18 @@
 package com.word.util;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ColorTest {
-
+    @Test
+    public void setFontColor_shouldContainOriginalText() {
+        String text = "hello world";
+        String greenText = Color.GREEN.setFontColor(text);
+        assertTrue(greenText.contains(text));
+        // regular text color.
+        System.out.println(text);
+        // should be green color.
+        System.out.println(greenText);
+    }
 }
