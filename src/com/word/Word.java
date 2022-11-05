6917d90a1d89ab6fd91b30ef721d6d7d4657765b
aeronin <aeroninouye@live.com>
2022-11-04 21:56:44 -1000

    Built out Player class and Word class. Finished falling method in Word class need to finish rest of business methods.

--- src/com/word/Word.java
+++ src/com/word/Word.java
@@ -1,38 +1,76 @@
 package com.word;
 
+import java.awt.*;
+
 class Word {
 
-    /**
-     * TODO: Aeron
-     *  Add attributes:
-     *      - int pointValue
-     *      - int fallingSpeed
-     *      - StringBuilder text
-     *      - Color color
-     *      - int xPos
-     *      - int yPos
-     *      - boolean isAlive
-     * */
+    private int pointValue;
+    private int fallingSpeed;
+    private StringBuilder text;
+    Color color;
+    private int xPos;
+    private int yPos;
+    boolean isAlive;
 
-    /**
-     * TODO:
-     *   Business Methods:
-     *    -fall()
-     *    -
-     * **/
+    //need to change String difficult to Enum difficulty.
+    public StringBuilder fall(String word, int fallingSpeed){
+            int randomNumberOfSpaces = (int) (Math.random() * 20 + 1);
+            StringBuilder line = new StringBuilder();
+            for (int i = 0; i < randomNumberOfSpaces; i++){
+                line.append(" ");
+            }
+            line.append(word);
+            if (fallingSpeed == 1){ //1 is easy
+                line.append("\n");
+            } else if (fallingSpeed == 2){ //2 is medium
+                line.append("\n\n");
+            } else {
+                line.append("\n\n\n");
+            }
+            return line;
+    }
 
-    /**
-     * TODO:
-     *  ACCESSORS:
-     *      Getters:
-     *      - getPointValue() but not setter
-     *      - getFallingSpeed() but not setter
-     *      - getText() but not setter
-     *      - getColor() but not setter
-     *     Setters:
-     *
-     *
-     * */
+    public void changeColor(){
+    }
+
+    public  void makeDisappear(){
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public int getFallingSpeed() {
+        return fallingSpeed;
+    }
+
+    public StringBuilder getText() {
+        return text;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
 
     /**
      * TODO:
--- src/com/word/client/Main.java
+++ src/com/word/client/Main.java
@@ -5,4 +5,6 @@ class Main {
      * TODO: Vlad
      * */
 
+
+
 }
