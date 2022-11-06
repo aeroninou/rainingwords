d6c21d4a4146d2704087b26ab9becc9b5f506b9d
Vladimir Antigua <vladimirantigua@gmail.com>
2022-11-06 15:38:41 -0800

    created 3 text files easy.txt medium.txt and hard.txt populated with 50 or more words read all the files for each of the files. created the junit test to test teh Difficulty brand.

--- src/com/word/Difficulty.java
+++ src/com/word/Difficulty.java
@@ -1,16 +1,65 @@
 package com.word;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
+/**
+ * TODO: vlad
+ *  - smart enum
+ *  create the 3 files
+ * */
 public enum Difficulty {
+
+    EASY("easy.txt"),
+    MEDIUM("medium"),
+    HARD("hard");
+
+    //advance enum use constructor and getter:
+    //Fields
+    private final String fileName;
+
+    //Constructor
+    Difficulty(String fileName){
+        this.fileName = fileName;
+    }
+
     /**
-     * TODO: vlad
-     *  - smart enum
-     *  create the 3 files
-     * */
-    EASY,
-    MEDIUM,
-    HARD
+     * TODO:
+     * read the file File.readAllLines
+     * returns string read in into Word objects
+     *  populate list of words
+     **/
+
+    //Method
 
 
+    //ACCESSOR:
+    public String getFileName() {
+        return fileName;
+    }
 
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+//        Path path = Paths.get("easy.txt");
+//        Path path = Paths.get("medium.txt");
+        Path path = Paths.get("hard.txt");
+
+        String content = null;
+        try {
+            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+            content = String.join(System.lineSeparator(), lines);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(content);
+    }
 }
