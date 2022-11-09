b3b9fb6107018562c2c6fb2d5839d1485678a5fa
Sergio Garcia <segarciat314@gmail.com>
2022-11-09 18:32:36 -0500

    Tested Menu.promptForDifficulty() for valid and invalid input. Tested Menu.welcome().

--- test/com/word/app/MenuTest.java
+++ test/com/word/app/MenuTest.java
@@ -1,6 +1,7 @@
 package com.word.app;
 
 import com.apps.util.Prompter;
+import com.word.Difficulty;
 import org.junit.Test;
 
 import java.io.File;
@@ -34,13 +35,29 @@ public class MenuTest {
     }
 
     @Test
-    public void promptForDifficulty_shouldCreateEnum_whenUserProvidesFirstLetterOfEnum() {
+    public void promptForDifficulty_shouldContinueToPrompt_whenUserProvidedInvalidInput() {
+        Menu.setScannerSource("responses/promptForDifficulty_invalidInput.txt");
+        assertEquals(Difficulty.EASY, Menu.promptForDifficulty());
+    }
 
+    @Test
+    public void promptForDifficulty_shouldCreateEnum_whenUserProvidesFirstLetterOfEnum() {
+        Menu.setScannerSource("responses/promptForDifficulty_valid.txt");
+        assertEquals(Difficulty.EASY, Menu.promptForDifficulty());
+        assertEquals(Difficulty.EASY, Menu.promptForDifficulty());
+        assertEquals(Difficulty.MEDIUM, Menu.promptForDifficulty());
+        assertEquals(Difficulty.MEDIUM, Menu.promptForDifficulty());
+        assertEquals(Difficulty.HARD, Menu.promptForDifficulty());
+        assertEquals(Difficulty.HARD, Menu.promptForDifficulty());
     }
 
     @Test
     public void welcome_shouldReadFileAndDisplayMessage_withoutIOException() {
-
+        try {
+            Menu.welcome();
+        } catch(Exception e) {
+            fail("Should not have thrown an exception. Is it unable to read the file?");
+        }
     }
 
     @Test
